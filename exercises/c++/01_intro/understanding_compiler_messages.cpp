3031d43e6e00901cdad96f269482d01c412d6af9
Militeee <militesalvatore@gmail.com>
2018-10-07 16:13:06 +0200

    Added compiler messages example file

---
+++ exercises/c++/01_intro/understanding_compiler_messages.cpp
@@ -0,0 +1,30 @@
+//#include <iostream>
+
+int add(int a, int b)
+{
+    int res;
+    add = a + b;
+    return res;
+}
+
+int main()
+{
+	// don't include <iostream>
+	// std::cout << "Hello world!";
+
+	// without semicolon
+	// std::cout << "Hello world!"
+
+	// wrong function name
+	// std::cOut << "Hello world!";
+
+	// typo in variable name
+	// intt a{1};
+
+	//illegal declaration
+	// double int{3.14}
+
+	//wrong argument to function
+	//int addition = add(1, "castoro");
+}
+
