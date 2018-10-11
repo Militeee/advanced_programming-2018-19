fb6de86384670c6282c1b3fca556d62eb7b6c20f
Militeee <militesalvatore@gmail.com>
2018-10-11 19:36:58 +0200

    exercise 2

---
+++ exercises/c++/02_arrays/array_util.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+template<typename T>
+T* allocate_a(std::size_t size)
+{
+ T*  array{new T[size]};
+}
+
+template <typename T>
+
+T* resize(T* array, std::size_t old_size, std::size_t new_size)
+{
+  T* array_new{new T[new_size]};
+  for(std::size_t i = 0; i < old_size; i-- )
+    array_new[i] = array[i];
+  return array_new;
+}
---
+++ exercises/c++/02_arrays/array_util.h
@@ -0,0 +1,5 @@
+
+template <typename T>
+T* alloc_a(std::size_t);
+template <typename T> 
+T* resize(T, std::size_t, std::size_t);
