de94075cc1a028d6a8bd033e2b43d68487d7331f
Militeee <militesalvatore@gmail.com>
2018-10-11 22:56:03 +0200

    All the exercises done, but not revised

--- exercises/c++/02_arrays/prime_numbers.cpp
+++ exercises/c++/02_arrays/prime_numbers.cpp
@@ -1,3 +1,11 @@
+/*
+  A program which calculate the first 100 prime numbers in a
+  naive way, storing them into an resizable array
+  @author Salvatore Milite
+  @versione 11 Octorber 2018
+
+ */
+
 #include <iostream>
 #include "array_util.h"
 
@@ -5,24 +13,26 @@
 int main()
 {
   std::size_t size = 2;
-  int* array{alloc_a<int>(size)};
+  int* array{allocate_a<int>(size)};
   unsigned int counter{0};
   for(unsigned int i = 2; i < 100; i++)
     {
       bool flag{true};
-      for(unsigned int j = 2; j < i && flag ; j++)
+	  unsigned int j;
+      for(j = 2; j < i && flag ; j++)
         if(i%j == 0)
-	  flag = false;
+			flag = false;
 
       if(flag)
-	array[counter++];
+		  array[counter++] = j;
 
-      if(counter >= size)
-	{
-	 array = resize<int>(array,size * 2, size);
-	 size *= 2; 
-	}
-    }
-  for(unsigned int i = 0; i < counter, i++)
-    std::out << array[i];
+      if(counter == size)
+	  {
+		  array = resize<int>(array,size, size*2);
+		  size *= 2; 
+	  }
+  }
+  for(unsigned int i = 0; i < counter; i++)
+	  std::cout << array[i] << std::endl;
+  delete[] array;
 }
