5f388465a73e7916071cf31310b0e7f629b5a7d0
Militeee <militesalvatore@gmail.com>
2018-11-11 19:21:30 +0100

    added comments

--- exercises/c++/04_custom_types/Date.cpp
+++ exercises/c++/04_custom_types/Date.cpp
@@ -1,22 +1,47 @@
+/*
+  A simple class to store the date
+  @author Salvatore Milie
+  @version 11 November 2018
+ */
+
 #include <iostream>
 
 namespace Date
 {
-
+  // enum that stores the months
   enum month_l{January, February, March, April, May,
 		June, July, August, September, October,
                 November, December};
+/* 
+   @class Date
+   @brief  or representing a date with year, month, day precision
+*/
+
   class Date
   {
     public:
 
-    int day() const {return _day;}
-    int year() const {return _year;}
-    month_l month() const {return _month;}
+    int day() const {return _day;} // day of the year
+    int year() const {return _year;} // year 
+    month_l month() const {return _month;} // month expressed in number
+    /*
+      @class Date
+      @brief a function for adding days
+      @details the function is a wrapper for multiple calls of add_days
+      @param n number of days to be added
+      @param[in] the date object that is gonna be modified
+     */
     void add_days(const unsigned int n) {
       for(int i = 0; i < n; i++)
 	this->add_day();
     }
+    /*
+      @class Date
+      @brief constructor for the class
+      @details the constructor does not check for correctness
+      of the numbers, it also serves as default constructor
+     */
+    
     Date(int day = 0, month_l month = January,
 	       int year = 0)
       :_day{day}, _month{month}, _year{year}
@@ -31,12 +56,24 @@ namespace Date
     int _year;
   };
 
-
+  /*
+    @brief controll if a year is leap
+    @param year the year as an int
+    @return a boolean value, True for a leap year, false otherwise
+   */
+  
   bool is_leap(const int yea)
   {
     return (yea % 4 == 0 && yea % 400 != 0 ? true : false);
   }
-  
+
+  /*
+    @class Date
+    @brief a function that returns how many date are in a month
+    @param _month the month expressed ad an initially defined enumerator's element
+    @return the number of days of that month
+   */
+
   int Date::control_nday() const
   {
     switch(_month)
@@ -49,6 +86,12 @@ namespace Date
       return 31;
     }
   }
+
+  /*
+    @class Month
+    @brief a function that add a month to a Date object
+    @param[in] the Date object to be updated
+  */
   void Date::add_month()
      {
        int mon = _month;
@@ -62,7 +105,11 @@ namespace Date
 	 _month = static_cast<month_l>(mon);}
        }
 	 
- 
+  /*
+    @class Month
+    @brief add a day to the Date object
+    @details calls add_month if the _day parameter is > of the one returned by control_nday()
+   */
   void Date::add_day()
   {
     _day++;
@@ -72,7 +119,11 @@ namespace Date
 	_day = 0;
       }
   }
-  
+  /*
+    @class Date
+    @brief overload of == operator for the class Date
+    @details two date objects are equal if they have all the 3 variables equal
+   */
   bool operator==(const Date& lhs, const Date& rhs)
   {
     if(lhs.day() == rhs.day() && lhs.month() == rhs.month()
@@ -82,12 +133,21 @@ namespace Date
 
     return false;
   }
-
+  /*
+    @class Date
+    @brief overload of != operator for the class Date
+    @details two date objects are not equal if they have all the 3 variables not equal
+   */ 
   bool operator!=(const Date& lhs, const Date& rhs)
   {
     return !(lhs == rhs);
   }
 
+  /*
+    @class Date
+    @brief overload of << operator for the class Date
+   */
+  
   std::ostream& operator<<(std::ostream& os, const
 			   Date& date)
   {
